d698a9a717a9b1614349eda74ab368f6ffce8312
Byfa <andreja19.nad@gmail.com>
2021-11-22 20:07:39 +0100

    Ex.3 Task 2 - Modifying the Index View to Provide a GUI for Creating Contacts

--- Begin/ControlManager/Controllers/ContactController.cs
+++ Begin/ControlManager/Controllers/ContactController.cs
@@ -22,5 +22,14 @@ namespace ControlManager.Controllers
         {
             return contactRepository.GetAllContacts();
         }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
     }
 }
